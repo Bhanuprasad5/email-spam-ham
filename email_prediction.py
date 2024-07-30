5cb801c563438219ca453e0c410f84b3d10905c5
Bhanu prasad Chouki <129620298+Bhanuprasad5@users.noreply.github.com>
2024-07-30 23:28:48 +0530

    Update email_prediction.py

--- email_prediction.py
+++ email_prediction.py
@@ -12,8 +12,8 @@ from sklearn.feature_extraction.text import CountVectorizer
 #image= Image.open(r"573326-innomatics_research_labs_logo.png")
 #st.image(image)
 # st.image(r"573326-innomatics_research_labs_logo.png")
-model = pickle.load(open(r"C:\Users\chouk\Streamlit\email_spam_ham\email spam ham.pkl","rb"))
-tf = pickle.load(open(r"C:\Users\chouk\Streamlit\email_spam_ham\vectorization.pkl","rb"))
+model = pickle.load(open(r"email spam ham.pkl","rb"))
+tf = pickle.load(open(r"vectorization.pkl","rb"))
 email = st.text_input("Enter the Email:")
 
 
