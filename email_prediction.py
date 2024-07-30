8cbedf1ec59727a243ce6f9e7320fa33c4a4adc9
Bhanu prasad Chouki <129620298+Bhanuprasad5@users.noreply.github.com>
2024-07-30 23:27:42 +0530

    Add files via upload

---
+++ email_prediction.py
@@ -0,0 +1,28 @@
+import pickle
+
+import pandas as pd
+import sklearn
+from PIL import Image
+from sklearn.naive_bayes import MultinomialNB
+
+import streamlit as st
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+#image= Image.open(r"573326-innomatics_research_labs_logo.png")
+#st.image(image)
+# st.image(r"573326-innomatics_research_labs_logo.png")
+model = pickle.load(open(r"C:\Users\chouk\Streamlit\email_spam_ham\email spam ham.pkl","rb"))
+tf = pickle.load(open(r"C:\Users\chouk\Streamlit\email_spam_ham\vectorization.pkl","rb"))
+email = st.text_input("Enter the Email:")
+
+
+
+
+
+if st.button("Submit"):
+    
+    data = tf.transform([email]).toarray()
+    
+    pred = model.predict(data)[0]
+    st.write("The Email is : ",pred)
